1fe1617eda61e4e049c32ec0fe19cb65c4d2a2d0
jose-rennyer <joselimao2002@gmail.com>
2022-02-08 08:30:12 -0300

    resolução de um bug e melhoria

--- script/crono.js
+++ script/crono.js
@@ -2,6 +2,9 @@ let s = 00
 let mi = 00
 let mil = 00
 let contar;
+let start = document.querySelector(".start_button_1")
+let pause = document.querySelector(".pause_button_2")
+let stop = document.querySelector(".stop_button_3")
 
 function zero(numero){
     if(numero < 10){
@@ -10,23 +13,27 @@ function zero(numero){
         return numero
     }
 }
+
 function iniciar(){ //botao de inicio
     contar = setInterval(contando, 10)
     document.querySelector(".title").style.color = "#0f0"
+    start.style.display = "none"
 }
 
 function pausar(){ //botao para pausar
     clearInterval(contar)
     document.querySelector(".title").style.color = "#f00"
+    start.style.display = "block"
 }
 
 function parar(){ //botao para parar
     clearInterval(contar)
     s=0
-    min=0
+    mi=0
     mil=0
     document.querySelector(".title").innerHTML = "00:00:00"
     document.querySelector(".title").style.color = "#000"
+    start.style.display = "block"
 }
 function contando(){
     mil++
@@ -39,3 +46,4 @@ function contando(){
     }
     document.querySelector(".title").innerHTML = `${zero(mi)}:${zero(s)}:${zero(mil)}` 
 }
+
